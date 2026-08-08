bb130e649c8e91435550a8568ab4315b82d1e957
Nunu00 <vpappaterra00@gmail.com>
2026-08-09 00:02:57 +0200

    Update incubation window to t <= 6 days in PrevisioneEngine.swift

--- FunghiCS/Services/PrevisioneEngine.swift
+++ FunghiCS/Services/PrevisioneEngine.swift
@@ -94,7 +94,7 @@ struct PrevisioneEngine {
         
         var probCalc = pMax * fattoreCampanaGauss * kVeg * kVento * kUmiditaSuolo
         
-        if rapportoPioggia >= 1.0 && t <= 3 {
+        if rapportoPioggia >= 1.0 && t <= 6 {
             probCalc = max(48.0, probCalc)
         }
         
